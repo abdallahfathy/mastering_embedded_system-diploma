de9765de452de3d9cee3a2c97ae06d47932b195f
abdullah <abdullahfathy97@gmail.com>
2022-02-10 14:08:24 +0200

    done platform_types

--- Assignments/Unit 3 Embedded C/Lesson1/Platform_types.h
+++ Assignments/Unit 3 Embedded C/Lesson1/Platform_types.h
@@ -1,2 +1,42 @@
-#include"stdbool"
-#include"stdint"
+
+/*
+ * platform_types.h
+ *
+ *  Created on: Feb 10, 2022
+ *      Author: pc
+ */
+
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+#include<stdint.h>
+
+
+typedef int8_t     sint8 ;
+typedef uint8_t    uint8;
+
+typedef	int16_t    sint16;
+typedef uint16_t   uint16;
+
+typedef int32_t    sint32;
+typedef uint32_t   uint32;
+
+typedef	int64_t    sint64 ;
+typedef uint64_t   uint64;	
+
+// volatile type 
+typedef volatile int8_t     vsint8 ;
+typedef volatile uint8_t    vuint8;
+
+typedef	volatile int16_t    vsint16;
+typedef volatile uint16_t   vuint16;
+
+typedef volatile  int32_t   vsint32;
+typedef volatile uint32_t   vuint32;
+
+typedef	volatile int64_t    vsint64 ;
+typedef volatile uint64_t   vuint64;	
+
+		
+		
+		
+#endif /* PLATFORM_TYPES_H_ */
